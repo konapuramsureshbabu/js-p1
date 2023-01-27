fb3f8a3b82df83927f1a880c2c53633c79c439b9
konapuramsureshbabu <konapuramsureshbabu@gmail.com>
2023-01-27 13:41:39 +0530

    first commit

---
+++ suresh.js
@@ -0,0 +1,7 @@
+let string1='rajaramraja'
+let string2='ramarajarama1223'
+if (string1.length>string2.length){
+    console.log(`${string1} length is more`)
+}else{
+    console.log(`${string2} length is more`)
+}
